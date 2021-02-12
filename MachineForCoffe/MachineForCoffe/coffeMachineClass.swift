a605c687b6b47531ee6bcc8db09b1811705758fa
Titanscull <ispetrovskyi@gmail.com>
2021-02-12 13:59:12 +0200

    Blank Start Project Created

---
+++ MachineForCoffe/MachineForCoffe/coffeMachineClass.swift
@@ -0,0 +1,8 @@
+//
+//  coffeMachineClass.swift
+//  MachineForCoffe
+//
+//  Created by Ievgen Petrovskiy on 12.02.2021.
+//
+
+import Foundation
---
+++ MachineForCoffe/MachineForCoffe/drinkClass.swift
@@ -0,0 +1,8 @@
+//
+//  drinkClass.swift
+//  MachineForCoffe
+//
+//  Created by Ievgen Petrovskiy on 12.02.2021.
+//
+
+import Foundation
